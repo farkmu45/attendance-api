9d7b738d02ea1a7f4ffea1ea8ae9cd37d28cb665
farkmu45 <farukmicros@outlook.com>
2024-03-27 09:58:52 +0800

    Disable user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'username' => 'farkmu45',
-        ]);
-
         Admin::factory()->create([
             'name' => 'Test Admin',
             'username' => 'admin',
